2bae6257ce0e98839635197f361005bcc9b501a4
SourabhEduco <sourabh.065@gmail.com>
2024-06-26 15:55:39 +0530

    Update app.js
    Hello mam please find my change

--- PA-EN_Chap 0/O0011Disc Ram/content_files/app.js
+++ PA-EN_Chap 0/O0011Disc Ram/content_files/app.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
 						//////////use "spaceclick" variable////////////////////////////////////////////////////////  
 					 window.abc=0;
 						   
-				      Local1=function(){}	
+				      Local1=function(){
+						console.log("Please add my shange")
+					  }	
 			     	  Local2=function(){}
 			    	  Local3=function(){}
 				      Local4=function(){}
