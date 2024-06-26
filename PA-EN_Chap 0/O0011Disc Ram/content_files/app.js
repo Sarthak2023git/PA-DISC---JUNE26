ac071977020983103a363bad1bfbd7744b0b3682
SourabhEduco <sourabh.065@gmail.com>
2024-06-26 15:44:10 +0530

    Update app.js
    Mam please the check the code

--- PA-EN_Chap 0/O0011Disc Ram/content_files/app.js
+++ PA-EN_Chap 0/O0011Disc Ram/content_files/app.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
 						//////////use "spaceclick" variable////////////////////////////////////////////////////////  
 					 window.abc=0;
 						   
-				      Local1=function(){}	
+				      Local1=function(){
+						  console.log("Add a new Function")
+						  }	
 			     	  Local2=function(){}
 			    	  Local3=function(){}
 				      Local4=function(){}
